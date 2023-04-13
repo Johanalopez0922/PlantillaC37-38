963a5602b99b242cf526004d5c417a69ec50f6c9
Johanalopez0922 <121140087+Johanalopez0922@users.noreply.github.com>
2023-04-13 10:28:42 -0700

    Add files via upload

--- js/Player.js
+++ js/Player.js
@@ -1,6 +1,23 @@
 class Player {
   constructor() {
-   
+   this.name = null; 
+   this.index = null; 
+   this.positionX = 0; 
+   this.positionX = 0; 
+  }
+
+  getCount(){
+    var playerCountRef = database.ref("playerCount"); 
+    playerCountRef.on("value", (data)=>{
+      playerCount = data.val(); 
+    })
+  }
+
+  //Recibe el parametro que va a actualizar, agregar count
+  updateCount(count){
+    database.ref("/").update({
+      playerCount: count
+    }); 
   }
 
   
