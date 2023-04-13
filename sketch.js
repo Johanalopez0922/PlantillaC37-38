e06b113ffd3defa9bdf50d562b94301a559b2588
Johanalopez0922 <121140087+Johanalopez0922@users.noreply.github.com>
2023-04-13 10:28:06 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,23 +2,49 @@ var canvas;
 var backgroundImage;
 var bgImg;
 var database;
-var form, player;
+//Agregar la variable gameState 
+var gameState; 
+var form, player, game;
 var playerCount;
 
+var car1_Img, car2_Img; 
+var car1, car2; 
+// Especificar el array 
+var cars = []
+var track; 
+
 function preload() {
-  backgroundImage = loadImage("./assets/background.png");
+  backgroundImage = loadImage("assets/background.png");
+  car1_Img = loadImage("assets/car1.png"); 
+  car2_Img = loadImage("assets/car2.png"); 
+  track = loadImage("assets/track.jpg"); 
+
 }
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   database = firebase.database();
-  game = new Game();
-  game.start();
-
+  
+  // form = new Form(); 
+  game = new Game(); 
+  // Llamar a la funcion getState
+  game.getState(); 
+  game.start(); 
+  bgImg = backgroundImage; 
+  
 }
 
 function draw() {
   background(backgroundImage);
+
+  if(playerCount=== 2){
+    game.update(1); 
+  }
+
+  if(gameState === 1){
+    //agregar los parentesis
+    game.play(); 
+  }
 }
 
 function windowResized() {
